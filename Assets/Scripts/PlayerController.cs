2b315a5e2cc2d2716f7b990a6ecb031022c57233
Ax1a <qflfernandez01@tip.edu.ph>
2022-11-13 20:14:57 +0800

    Update Player Controls
    Disable control when UI is enabled. Better control based on the camera angle.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,20 +10,23 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-
+    public GameObject panelCom;
+    
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (panelCom.activeInHierarchy == false) {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            controller.Move(direction * speed * Time.deltaTime);
+            if(direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+                
+                controller.Move(direction * speed * Time.deltaTime);
+            }
         }
     }
 }
