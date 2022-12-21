0f5da1e83e5a6fc4c563d8f3dd5d537911c576de
Ax1a <qflfernandez01@tip.edu.ph>
2022-12-21 18:40:53 +0800

    Energy and Models Update
    - Reset energy when sleeping
    - Change model for bed
    - Removed unnecessary objects
    - Fixed shop button

--- Assets/Scripts/InteractionSystem/Bed.cs
+++ Assets/Scripts/InteractionSystem/Bed.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Bed : MonoBehaviour, Interactable
 {
     Clock _clock;
+    Energy _energy;
     [SerializeField] GameObject _time;
+    [SerializeField] GameObject EnergyPanel;
     [SerializeField] private string _prompt;
+    
     public string InteractionPrompt => _prompt;
 
     public bool Interact(InteractObject interactor)
     {
         _clock = _time.GetComponent<Clock>();
-
+        _energy = EnergyPanel.GetComponent<Energy>();
+        
+        _energy.ResetEnergy();
         _clock.Minute = 0;
         _clock.Hour = 7;
         _clock.Day += 1;
