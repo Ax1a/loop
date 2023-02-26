af13085ee310dba083c2eb712c0bc9bc20b7e162
XceedGone <83834461+XceedGone@users.noreply.github.com>
2023-02-27 04:37:34 +0800

    ready for checking

--- Assets/Scripts/Blocks/validate.cs
+++ Assets/Scripts/Blocks/validate.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class validate : MonoBehaviour
 {
     private TMP_InputField input;
+    public GameObject errorMessage;
     public TextMeshProUGUI output;
     public Transform simpleBlock;
     public Button btn;
@@ -14,7 +15,26 @@ public class validate : MonoBehaviour
     public void valid()
     {
         input = simpleBlock.GetComponentInChildren<TMP_InputField>();
+        if(input.text.Length != 0)
+        {
         output.text = input.text;
         btn.transform.gameObject.SetActive(true);     
+        }
+        else
+        {
+        errorMessage.SetActive(true);
+        Debug.Log("Error Message");
+        
+        }
+    }
+
+    public void removeError()
+    {
+        if(errorMessage.activeSelf)
+        {
+        errorMessage.SetActive(false);
+
+        }
+
     }
 }
