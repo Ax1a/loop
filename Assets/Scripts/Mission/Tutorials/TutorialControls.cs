34afbf44c20dbe78d2721c937176c91c991e85a5
Francis Fernandez <francisfernandez0679@gmail.com>
2023-03-21 01:17:48 +0800

    Added save for quest progress
    Co-Authored-By: XceedGone <83834461+XceedGone@users.noreply.github.com>
    Co-Authored-By: Drezza Anacito <28000565+blazingkyuubii@users.noreply.github.com>

--- Assets/Scripts/Mission/Tutorials/TutorialControls.cs
+++ Assets/Scripts/Mission/Tutorials/TutorialControls.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,17 +8,25 @@ public class TutorialControls : Tutorial
     public List<string> Keys = new List<string>();
     [SerializeField] private GameObject parent;
     [SerializeField] private GameObject highlightGuide;
+    private List<string> _keyCountTemp;
+
+    private void OnEnable() {
+        _keyCountTemp = Keys.ToList();
+    }
 
     public override void CheckIfHappening()
     {
-        for (int i = 0; i < Keys.Count; i++) {
-            if (Input.inputString.Contains(Keys[i])) {
+        for (int i = 0; i < _keyCountTemp.Count; i++) {
+            if (Input.inputString.Contains(_keyCountTemp[i])) {
                 // Change the toggle box to check
                 Transform _panel = parent.gameObject.transform.GetChild(0);
                 QuestUISidePanel _questUI = _panel.GetComponent<QuestUISidePanel>();
                 _questUI.ToggleCheck(true, i);
-                //
-                
+            }
+        }
+
+        for (int i = 0; i < Keys.Count; i++) {
+            if (Input.inputString.Contains(Keys[i])) {
                 AudioManager.Instance.PlaySfx("Success");
                 Keys.RemoveAt(i);
 
