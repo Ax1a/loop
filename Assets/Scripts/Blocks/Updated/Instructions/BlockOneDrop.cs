60a264462b9abaa152f9ae7e108ab56fda09581d
Francis Fernandez <francisfernandez0679@gmail.com>
2023-04-16 16:47:08 +0800

    Updated shuffle quiz

--- Assets/Scripts/Blocks/Updated/Instructions/BlockOneDrop.cs
+++ Assets/Scripts/Blocks/Updated/Instructions/BlockOneDrop.cs
@@ -44,12 +44,9 @@ public class BlockOneDrop : BlockDrag
 
     public override void BlockValidation()
     {
-        if (dropBlock.transform.childCount == 0) {
-            consoleValue = "";
-            return;
-        };
         if (_dropZone == null) return; // Don't check the validation when not on the drop block
         
+        Debug.Log(gameObject.name);
         foreach (var dropID in _dropZone.GetComponent<BlockDrop>().ids)
         {
             if (dropID == id)
@@ -59,7 +56,6 @@ public class BlockOneDrop : BlockDrag
                 {
                     if (!addedPoints && addPoints)
                     {
-                        Debug.Log("added points");
                         validationManager.AddPoints(1);
                         addedPoints = true;
                     }
@@ -68,33 +64,39 @@ public class BlockOneDrop : BlockDrag
                 {
                     if (addedPoints && addPoints)
                     {
-                        Debug.Log("reduced points");
                         validationManager.ReducePoints(1);
                         addedPoints = false;
                     }
                 }
-                if (blockType == BlockType.Type2) {
-                    BlockDrag blockDrag = dropBlock.transform.GetChild(0).GetComponent<BlockDrag>();
-                    if (blockDrag != null && blockDrag.consoleValue.Length != 0 && blockDrag.consoleValue != consoleValue) {
-                        consoleValue = blockDrag.consoleValue;
-                    }
-                }
-                else if (blockType == BlockType.Type1) {
-                    BlockDrag blockDrag = dropBlock.transform.GetChild(0).GetComponent<BlockDrag>();
-                    if (blockDrag != null && blockDrag.consoleValue.Length != 0 && blockDrag.consoleValue != consoleValue) {
-                        consoleValue = blockDrag.consoleValue;
-                    }
-                    // else if (gameObject.name.StartsWith("CharInput")) {
-                    //     BlockVariable blockVariable = dropBlock.transform.GetChild(0).GetComponent<BlockVariable>();
-                    //     BlockInput blockInput = dropBlock.transform.GetChild(0).GetComponent<BlockInput>();
 
-                    //     if (blockVariable != null) {
-                    //         validationManager.AskForInput(blockVariable);
-                    //     }
-                    //     else if (blockInput != null && blockInput.consoleValue.Length != 0) {
-                    //         blockInput.consoleValue = "";
-                    //     }
+                if (dropBlock.transform.childCount > 0) {
+                    // if (blockType == BlockType.Type2) {
+                        BlockDrag blockDrag = dropBlock.transform.GetChild(0).GetComponent<BlockDrag>();
+                        if (blockDrag != null && blockDrag.consoleValue != consoleValue) {
+                            consoleValue = blockDrag.consoleValue;
+                        }
                     // }
+                    // else if (blockType == BlockType.Type1) {
+                        // BlockDrag blockDrag = dropBlock.transform.GetChild(0).GetComponent<BlockDrag>();
+                        // if (blockDrag != null && blockDrag.consoleValue.Length != 0 && blockDrag.consoleValue != consoleValue) {
+                        //     consoleValue = blockDrag.consoleValue;
+                        // }
+                        // else if (gameObject.name.StartsWith("CharInput")) {
+                        //     BlockVariable blockVariable = dropBlock.transform.GetChild(0).GetComponent<BlockVariable>();
+                        //     BlockInput blockInput = dropBlock.transform.GetChild(0).GetComponent<BlockInput>();
+
+                        //     if (blockVariable != null) {
+                        //         validationManager.AskForInput(blockVariable);
+                        //     }
+                        //     else if (blockInput != null && blockInput.consoleValue.Length != 0) {
+                        //         blockInput.consoleValue = "";
+                        //     }
+                        // }
+                    // }
+                }
+                else {
+                    consoleValue = "";
+                    return;
                 }
                 // if (addConsoleValue) consoleValue = inputField.text;
                 return;
