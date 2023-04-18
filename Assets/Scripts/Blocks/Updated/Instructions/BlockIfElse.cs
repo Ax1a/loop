6cdd64fd3cda64d5112e549ce9731a7ff18701ca
Francis Fernandez <francisfernandez0679@gmail.com>
2023-04-18 11:23:34 +0800

    dsad
    Co-Authored-By: XceedGone <83834461+XceedGone@users.noreply.github.com>
    Co-Authored-By: Drezza Anacito <drezzaanacito0@gmail.com>

--- Assets/Scripts/Blocks/Updated/Instructions/BlockIfElse.cs
+++ Assets/Scripts/Blocks/Updated/Instructions/BlockIfElse.cs
@@ -7,58 +7,64 @@ public class BlockIfElse : BlockDrag
     public GameObject dropBlock;
     public GameObject ifChildContainer;
     public GameObject elseChildContainer;
-    private int childCount;
+    [SerializeField] private string answer;
+    BlockDrag blockDrag = null;
 
     public override void Update() {
         base.Update();
         
-        if (dropBlock.transform.childCount != childCount) {
-            childCount = dropBlock.transform.childCount;
-            inputChanged = true;
+        if (dropBlock.transform.childCount > 0) {
+            blockDrag = dropBlock.transform.GetChild(0)?.GetComponent<BlockDrag>();
+
+            if (blockDrag.consoleValue != consoleValue) {
+                consoleValue = blockDrag.consoleValue;
+                inputChanged = true;
+            }
         }
+    }
+
+    private bool ValidateInput() {
+        return answer.ToLower() == consoleValue.ToLower();
     }
 
     public override void BlockValidation()
     {
         if (_dropZone == null) return; // Don't check the validation when not on the drop block
-        if (dropBlock.transform.childCount == 0) return;
+        if (dropBlock.transform.childCount == 0 || !inputChanged) return;
 
-        BlockOperator blockOperator = dropBlock.GetComponent<BlockOperator>();
-
-        // if (blockOperator.consoleValue == "true") {
-        //     consoleValue = "true";
-        // }
-        // else {
-        //     consoleValue = "false";
-        // }
+        blockDrag = dropBlock.transform.GetChild(0)?.GetComponent<BlockDrag>();
+        consoleValue = (blockDrag?.consoleValue == "true") ? "true" : "false";
 
         foreach (var dropID in _dropZone.GetComponent<BlockDrop>().ids)
         {
             if (dropID == id)
             {
-                // if (addConsoleValue) consoleValue = inputField.text;
+                error = false;
 
-                // if (ValidateInput())
-                // {
+                if (ValidateInput())
+                {
                     if (!addedPoints)
                     {
                         validationManager.AddPoints(1);
                         addedPoints = true;
                     }
-                // }
-                // else
-                // {
-                    // if (addedPoints)
-                    // {
-                    //     validationManager.ReducePoints(1);
-                    //     addedPoints = false;
-                    // }
-                // }
+                }
+                else
+                {
+                    if (addedPoints)
+                    {
+                        validationManager.ReducePoints(1);
+                        addedPoints = false;
+                    }
+                }
+                inputChanged = false;
                 return;
             }
             else {
                 error = true;
             }
         }
+
+        inputChanged = false;
     }
 }
