5783ea797dc60c03fdd35e31802c03798ffccbf7
XceedGone <83834461+XceedGone@users.noreply.github.com>
2023-05-13 22:39:53 +0800

    MAZE
    - have issue

--- Assets/Scripts/MazeSystem/CollectibleBlock.cs
+++ Assets/Scripts/MazeSystem/CollectibleBlock.cs
@@ -15,30 +15,31 @@ public class CollectibleBlock : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        mazeManager = other.GetComponent<MazeManager>();
-        if (mazeManager != null)
+        if (other.gameObject.tag == "Player")
         {
-            if (QuizPanel != null)
+            // mazeManager = other.GetComponent<MazeManager>();
+            if (mazeManager != null)
             {
-                if (!QuizPanel.activeSelf) // check if quiz panel is not active
+                if (QuizPanel != null)
                 {
-                    QuizPanel.SetActive(true);
-                    gameObject.SetActive(false);
-                }
-                else if (mazeQuizManager.currentPoints == mazeQuizManager.pointsToWin && !collected) // check if player has answered the quiz
-                {
-                    collected = true;
-                    mazeQuizManager.Correct(); // trigger the correct answer logic
-                }
-                else
-                {
-                    Debug.Log("try again");
+                    if (!QuizPanel.activeSelf) // check if quiz panel is not active
+                    {
+                        QuizPanel.SetActive(true);
+                        gameObject.SetActive(false);
+                    }
+                    else if (mazeQuizManager.currentPoints == mazeQuizManager.pointsToWin && !collected) // check if player has answered the quiz
+                    {
+                        collected = true;
+                        mazeQuizManager.Correct(); // trigger the correct answer logic
+                    }
+                    else
+                    {
+                        Debug.Log("try again");
+                    }
                 }
             }
         }
     }
-
-
     public void Collect()
     {
         if (!collected)
@@ -52,10 +53,8 @@ public class CollectibleBlock : MonoBehaviour
             Debug.Log("Collected");
         }
     }
-
     public void NoTCollected()
     {
         gameObject.SetActive(true);
-
     }
 }
